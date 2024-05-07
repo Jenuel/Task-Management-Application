16dd9085cf42f4a07b3413383d18f8b25bf6e65e
Renuel Balogo <2230174@slu.edu.ph>
2024-05-07 21:05:26 +0800

    Created the implementation of the authentication

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false)
+
+  const handleLoggingIn = () => {
+    setAuthenticated(true)
+  }
+
   return (
     <div className="App">
-      
+      {!authenticated ? (
+        <LoginPage login={handleLoggingIn}/>
+      ) : (
+
+      )}
     </div>
   );
 }
