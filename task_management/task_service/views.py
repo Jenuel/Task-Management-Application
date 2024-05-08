0411cb01bf26af9090f34fa33fb096ce1458dea3
Renuel Balogo <2230174@slu.edu.ph>
2024-05-08 23:07:41 +0800

    Made the API request types

--- task_management/task_service/views.py
+++ task_management/task_service/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import generics, status
+from .serializers import serializers
+from rest_framework.decorators import api_view
 from .models import Task
 
-def task_list(request):
+import requests
+
+@api_view(['GET'])
+def allTask(request):
     tasks = Task.objects.all()
     data = {'tasks': list(tasks.values())}
     return JsonResponse(data)
 
-def create_task(request):
-    # Handle POST request to create a new task
+@api_view(['POST'])
+def add_task(request):
     pass
 
+@api_view(['GET'])
+def read_task(request):
+
+    pass
+
+@api_view(['PUT'])
 def update_task(request, task_id):
     # Handle PUT request to update an existing task
     pass
 
+@api_view(['DELETE'])
 def delete_task(request, task_id):
     # Handle DELETE request to delete a task
     pass
