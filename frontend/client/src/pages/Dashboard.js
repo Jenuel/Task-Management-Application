4fbd663683261524803c15fe53e9529307deecce
Renuel Balogo <2230174@slu.edu.ph>
2024-05-09 20:47:08 +0800

    Fetch the data and made the task clickable

--- frontend/client/src/pages/Dashboard.js
+++ frontend/client/src/pages/Dashboard.js
@@ -1,21 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Dashboard() {
-  const [tasks, setTasks] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [error, setError] = useState('')
+
+
+  useEffect(() => {
+    fetchTasks
+  }, [])
+
+  //fetching of data
+  const fetchTasks = () => {
+    axios.get('/tasks/')
+    .then(response => {
+      setTasks(response.data)
+    })
+    .catch(error => {
+      setError("Server error")
+    })
+  }
+
+
+  const handleClick = (taskId) => {
+      //popup
+  }
+
+  const handleAddButton = () => {
+
+  }
   
   return (
     <div className='dashboard'>
       <div className='navbar'>
         <h1 className="app-name">Task Management Application</h1>
-        <button className="add-task">Add task</button>
+        <button className="add-task" onClick={handleAddButton}>Add task</button>
       </div>
       <div className='task-container'>
-        <button className="filter">Filter</button>
+        {/* <select value={sortOption} onChange={handleSort}>
+          <option value="pending">Sort by Title</option>
+          <option value="status">Sort by Status</option>
+        </select> */}
         <h4>Tasks</h4>
-        {tasks.map(task => (
-          <Task key={task.id} task={task}/>  
-        ))}
-
+        {error ? (
+          <div className="error">{error}</div>
+        ) : (
+          tasks.map(task => (
+          <Task task={task} onClick={handleClick}/>  
+          ))
+        )}
       </div>
     </div>
   )
