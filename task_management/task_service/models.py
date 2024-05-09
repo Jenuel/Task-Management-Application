2988fbc404e8393d08629fbcf1cc240e4fec4e43
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-05-09 18:33:36 +0800

    testing

--- task_management/task_service/models.py
+++ task_management/task_service/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Task(models.Model):
+    #id = models. testing
     title = models.CharField(max_length=100)
     description = models.TextField()
     status = models.CharField(max_length=20, default='Pending')
+    acc_id = models.IntegerField()
+
 
     def __str__(self):
         return self.title
