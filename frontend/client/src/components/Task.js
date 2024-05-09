67c2da4f9ebdf60f9f9a74c09e6512cd30820abf
Renuel Balogo <2230174@slu.edu.ph>
2024-05-09 20:44:53 +0800

    Added necessary buttons for the CRUD operations

--- frontend/client/src/components/Task.js
+++ frontend/client/src/components/Task.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-function Task({key, task}) {
-    
+function Task({task, onClick, currentAccount}) {
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = () => {
+
+    }
+
+    const handleUpdateStatus = () => {
+
+    }
+
+
   return (
-    <div className='task'>
+    <div className='task' onClick={() => onClick(task.id)}>
         <h2>{task.title}</h2>
         <h4>{task.status}</h4>
+        <h4>{task.acc_id}</h4>
+        {currentAccount.id === task.acc_id && (
+          <>
+            <button className="edit" onClick={handleEdit}>Edit</button>
+            <button className="delete" onClick={handleDelete}>Delete</button>
+          </>
+        )}
+        <button className="complete" onClick={handleUpdateStatus}>Completed</button>
     </div>
   )
 }
