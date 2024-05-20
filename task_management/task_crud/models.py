87080d125f027f7457e47c6d705f92f8d09b26f4
Renuel Balogo <renueljeremibalogo@gmail.com>
2024-05-20 13:51:26 +0800

    Model relocation

--- task_management/task_crud/models.py
+++ task_management/task_crud/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    #id = models. testing
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=20, default='Pending')
+    acc_id = models.IntegerField()
+
+
+    def __str__(self):
+        return self.title
